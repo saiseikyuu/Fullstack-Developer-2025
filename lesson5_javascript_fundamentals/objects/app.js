47cda20b2921fa74b39551144eaee0148fdc9351
saiseikyuu <saiseikyuu@gmail.com>
2025-10-22 18:51:00 +0800

    Update objects

---
+++ lesson5_javascript_fundamentals/objects/app.js
@@ -0,0 +1,16 @@
+// objects
+
+
+let person ={
+    name: "Rain",
+    age:23,
+    country: "Philippines"
+}
+
+
+function greetUser(){
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country);
+}
+
+
+greetUser();
