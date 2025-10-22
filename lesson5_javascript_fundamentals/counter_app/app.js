25d4eb807abe24c199d8dbf47ea59111b6e45420
saiseikyuu <saiseikyuu@gmail.com>
2025-10-22 15:39:10 +0800

    Update save button in counter app

--- lesson5_javascript_fundamentals/counter_app/app.js
+++ lesson5_javascript_fundamentals/counter_app/app.js
@@ -1,15 +1,27 @@
 
+let countElement = document.getElementById("count-element");
+let saveElement = document.getElementById("save-element");
+
 
 let count = 0;
-let countElement = document.getElementById("count");
+
+console.log(saveElement)
 
 function increment(){
     count += 1;
     countElement.innerText = count;
+
 }
 
 
 function decrement(){
     count -= 1;
     countElement.innerText = count;
+}
+
+function save(){
+    let countStr = count + " - ";
+    saveElement.textContent += countStr
+    countElement.textContent = 0;
+    count = 0;
 }
