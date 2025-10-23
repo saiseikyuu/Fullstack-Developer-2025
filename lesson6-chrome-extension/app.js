a9d0f92b4b78c53451d5ccb61a69ffc3ef739a4b
saiseikyuu <saiseikyuu@gmail.com>
2025-10-23 23:28:13 +0800

    Update

--- lesson6-chrome-extension/app.js
+++ lesson6-chrome-extension/app.js
@@ -1,3 +1,26 @@
-function saveLead (){
-    console.log("Lead saved!");
+
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    renderLeads();
+    inputEl.value = "";
+
+})
+
+function renderLeads(){
+    let listItems = "";
+    for (let lead in myLeads){
+        // listItems += "<li><a target='_blank' href='" + myLeads[lead] + "'>" + myLeads[lead] + "</a></li>";
+    listItems += `
+    <li>
+         <a target='_blank' href='${myLeads[lead]}'>${myLeads[lead]}</a>
+    </li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
+
