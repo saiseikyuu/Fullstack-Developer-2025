f1dc4714a764f84be2ed288d6476534de39332f4
saiseikyuu <saiseikyuu@gmail.com>
2025-10-24 11:17:23 +0800

    Update classes

---
+++ lesson8-classes/app.js
@@ -0,0 +1,15 @@
+class Person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+
+
+    greet(){
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+const Rain = new Person("Rain", 23);
+
+Rain.greet();
