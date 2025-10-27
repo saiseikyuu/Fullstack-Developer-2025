b83ea51bfc22dce9efb83cb954fe2f9307ed373a
saiseikyuu <saiseikyuu@gmail.com>
2025-10-27 22:01:35 +0800

    Update: First http request

--- lesson11-nodejs/server.js
+++ lesson11-nodejs/server.js
@@ -1,1 +1,13 @@
-console.log("Starting server...");
+import http from 'node:http';
+
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from Node.js server!');
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
