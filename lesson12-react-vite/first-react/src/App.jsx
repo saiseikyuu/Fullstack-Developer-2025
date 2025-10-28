c09e8e69dac9e01af6083ebb75993533f47b2361
saiseikyuu <saiseikyuu@gmail.com>
2025-10-28 18:38:57 +0800

    Update React fun fact

--- lesson12-react-vite/first-react/src/App.jsx
+++ lesson12-react-vite/first-react/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import reactSVG from './assets/react.svg'
 
 function App() {
 
   return (
     <>
-      <h1>Hello, React with Vite!</h1>      
+      <img src={reactSVG}  alt="" />
+      <h1>Fun Facts About React!</h1>      
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quisquam?</li>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, nemo? Aliquam quos dolorum facere natus.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, numquam.</li>
+      </ul>
     </>
   )
 }
