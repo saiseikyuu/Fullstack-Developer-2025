6eb62daad1fc98f6df17ebbde5b922a1da56be71
saiseikyuu <saiseikyuu@gmail.com>
2025-10-29 18:27:12 +0800

    Update props

---
+++ lesson13-travel-app/travel-app/components/Entry.jsx
@@ -0,0 +1,9 @@
+
+export default function Entry(props) {
+    return (
+        <>
+          <h1>{props.country}</h1>
+        
+        </>
+    )
+}
--- lesson13-travel-app/travel-app/src/App.jsx
+++ lesson13-travel-app/travel-app/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
 import Header from  '../components/Header'
+import Entry from '../components/Entry'
+import data from '../data.js'
+
+
+
+
 
 function App() {
+
+  const entryElements = data.map(entry => {
+    return (
+      <Entry key={entry.id} country={entry.country} />
+    )
+  })
+
 
   return (
     <>
       <Header />
+      {entryElements}
+
 
     </>
   )
