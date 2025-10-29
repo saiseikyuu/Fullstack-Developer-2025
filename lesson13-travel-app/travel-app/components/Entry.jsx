33c97589931ecb260fce52410b10539f229cc69e
saiseikyuu <saiseikyuu@gmail.com>
2025-10-29 18:35:42 +0800

    Update date

--- lesson13-travel-app/travel-app/components/Entry.jsx
+++ lesson13-travel-app/travel-app/components/Entry.jsx
@@ -5,6 +5,7 @@ export default function Entry(props) {
           <h1>{props.country}</h1>
           <p>{props.title}</p>
           <img src={props.img.src} alt={props.img.alt} />
+          <p>{props.dates}</p>
           <p>{props.text}</p>
         
         </>
