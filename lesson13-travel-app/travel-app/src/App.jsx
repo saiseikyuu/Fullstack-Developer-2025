827a72332f9a8eacce08d5b388168907d15c86a4
saiseikyuu <saiseikyuu@gmail.com>
2025-10-30 23:04:08 +0800

    Update

--- lesson13-travel-app/travel-app/src/App.jsx
+++ lesson13-travel-app/travel-app/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <Entry 
        key={entry.id}
        country={entry.country}  
-       title={entry.title}
        dates={entry.dates}
        text={entry.text}
        img={entry.img}
